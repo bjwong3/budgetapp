799439c1cf555a58f71b90b24fe1775968c1fa31
Brandon <brandonjwong@gmail.com>
2024-08-02 21:39:22 -0500

    Display monthly income and expense

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const DataDisplay = ({ data, updateData, addToArray}) => {
-  const [editKey, setEditKey] = useState('');
   const [incomeValue, setIncomeValue] = useState('');
   const [expenseValue, setExpenseValue] = useState('');
+  const [totalExpense, setTotalExpense] = useState(0);
   const incomeKey = "income";
   const expenseKey = "expense";
 
   const updateIncome = () => {
     
-    if (incomeValue !== undefined && !isNaN(incomeValue)) {
+    if (incomeValue !== '' && incomeValue !== undefined && !isNaN(incomeValue)) {
       updateData(incomeKey, incomeValue);
       setIncomeValue('');
     }
@@ -26,7 +27,7 @@ const DataDisplay = ({ data, updateData, addToArray}) => {
   };
 
   const addToExpense = () => {
-    if (expenseValue !== undefined && !isNaN(expenseValue)) {
+    if (expenseValue !== '' && expenseValue !== undefined && !isNaN(expenseValue)) {
       addToArray(expenseKey, expenseValue);
       setExpenseValue('');
     }
@@ -42,8 +43,37 @@ const DataDisplay = ({ data, updateData, addToArray}) => {
     }
   };
 
+  const renderTable = (array) => {
+    return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {array.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  const sumExpenses = () => {
+    let sum = 0;
+    data[expenseKey].forEach(element => {
+      sum += parseInt(element);
+    });
+    return sum;
+  }
+
   return (
-    <div>
+    <div className="container mt-4">
       <h1>Data Display</h1>
       <textarea
         readOnly
@@ -51,28 +81,40 @@ const DataDisplay = ({ data, updateData, addToArray}) => {
         rows={10}
         cols={50}
       />
+      <h2 class='text-success'>Monthly Income: {data[incomeKey]}</h2>
+      <h2 class='text-danger'>Monthly Expenses: {sumExpenses()}</h2>
+      
 
       <div>
         Monthly Income:  &nbsp;
         <input
           type="text"
+          className="form-control"
           placeholder="Value"
           value={incomeValue}
           onChange={handleIncomeValue}
         />
-        <button onClick={updateIncome}>Confirm</button>
+        <button className="btn btn-primary" onClick={updateIncome}>Confirm</button>
       </div>
 
       <div>
         Monthly Expense:  &nbsp;
         <input
           type="text"
+          className="form-control"
           placeholder="Value"
           value={expenseValue}
           onChange={handleExpenseValue}
         />
-        <button onClick={addToExpense}>Add to Expenses</button>
+        <button className="btn btn-primary" onClick={addToExpense}>Add to Expenses</button>
       </div>
+
+      {Array.isArray(data[expenseKey]) && (
+        <div>
+          <h2>List of Expenses</h2>
+          {renderTable(data[expenseKey])}
+        </div>
+      )}
     </div>
   );
 };
